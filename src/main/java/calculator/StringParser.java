ffd7bb65f20d71cc0fe0d3fe259bc7d61d9f8189
codms0201 <chanyeol0615@naver.com>
2025-10-20 19:14:19 +0900

    feat(domain): 콜론 구분자 처리 기능 추가

--- src/main/java/calculator/StringParser.java
+++ src/main/java/calculator/StringParser.java
@@ -2,6 +2,6 @@ package calculator;
 
 public class StringParser {
     public static String[] parseBasic(String input) {
-        return input.split(",", -1);
+        return input.split("[,:]", -1);
     }
 }
